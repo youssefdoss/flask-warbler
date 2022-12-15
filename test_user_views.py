0eec7e6d4dd674f28a948c65f585c6b0c9bc42dc
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-12-15 17:53:45 -0600

    more tests

--- test_user_views.py
+++ test_user_views.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, Message, User, connect_db
+from models import db, Message, User, connect_db, Follows
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -168,7 +168,7 @@ class UserAuthorizationTestUseCase(UserBaseViewTestCase):
             self.assertIn("Username or email already taken", html)
 
     def test_registration_duplicate_email(self):
-        '''Tests that correct HTML is returning when signing up with duplicate user'''
+        '''Tests that correct HTML is returning when signing up with duplicate email'''
         with self.client as c:
             data = {"username": "foo", "password": "password",
                 "email": "u2@email.com", "image_url": None}
@@ -200,7 +200,7 @@ class UserProfileViewTestCase(UserBaseViewTestCase):
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Access unauthorized.', html)
-    
+
     def test_list_users(self):
         '''Tests that route shows all users on the database'''
         with self.client as c:
@@ -212,7 +212,7 @@ class UserProfileViewTestCase(UserBaseViewTestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn('@u1', html)
             self.assertIn('@u2', html)
-    
+
     def test_list_users_not_logged_in(self):
         '''Tests that list users returns correct html when logged out'''
         with self.client as c:
@@ -257,17 +257,168 @@ class UserProfileViewTestCase(UserBaseViewTestCase):
                 sess[CURR_USER_KEY] = self.u1_id
             resp = c.post('/users/delete', follow_redirects=True)
             html = resp.get_data(as_text=True)
-            
+
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Join Warbler today.', html)
             user = User.query.get(self.u1_id)
             self.assertIsNone(user)
-    
+
     def test_delete_profile_not_logged_in(self):
         '''Tests that deleting a profile returns the correct html when not logged in'''
         with self.client as c:
             resp = c.post('/users/delete', follow_redirects=True)
             html = resp.get_data(as_text=True)
-            
+
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Access unauthorized.', html)
+
+class LikeViewTestCase(UserBaseViewTestCase):
+    '''Tests for like view functions'''
+    def setUp(self):
+        super().setUp()
+
+        m1 = Message(text = "text", user_id = self.u2_id)
+        m2 = Message(text = "text2", user_id = self.u2_id)
+        u1 = User.query.get(self.u1_id)
+
+        u1.liked_messages.append(m1)
+
+        db.session.add_all([m1, m2])
+        db.session.commit()
+
+        self.m1_id = m1.id
+        self.m2_id = m2.id
+
+    def test_add_like(self):
+        '''Tests to show that proper HTML is returned when liking message'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            data = {"location" : f"/messages/{self.m2_id}"}
+            resp = c.post(f'/messages/{self.m2_id}/like', data=data,
+                                             follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            user = User.query.get(self.u1_id)
+            self.assertIn("bi bi-heart-fill", html)
+            self.assertEqual(len(user.liked_messages), 2)
+
+    def test_add_like_as_guest(self):
+        '''Tests to show that proper HTML is returned when liking message as guest'''
+        with self.client as c:
+
+            data = {"location" : f"/messages/{self.m2_id}"}
+            resp = c.post(f'/messages/{self.m2_id}/like', data=data,
+                                             follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            user = User.query.get(self.u1_id)
+            self.assertIn("Access unauthorized", html)
+            self.assertEqual(len(user.liked_messages), 1)
+
+    def test_remove_like(self):
+        '''Tests to show that proper HTML is returned when liking message'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            data = {"location" : f"/messages/{self.m1_id}"}
+            resp = c.post(f'/messages/{self.m1_id}/like', data=data,
+                                             follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            user = User.query.get(self.u1_id)
+            self.assertIn("bi bi-heart", html)
+            self.assertEqual(len(user.liked_messages), 0)
+
+    def test_show_likes(self):
+        '''Test to show that show likes function returns correct HTML'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get(f'/users/{self.u1_id}/likes', follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u2", html)
+            self.assertEqual(resp.status_code, 200)
+
+
+    def test_show_likes_as_guest(self):
+        '''Test to show that show likes function returns correct HTML when not logged in'''
+        with self.client as c:
+            resp = c.get(f'/users/{self.u1_id}/likes', follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("Access unauthorized", html)
+            self.assertEqual(resp.status_code, 200)
+
+class FollowUserViewTestCase(UserBaseViewTestCase):
+    '''Tests to verify that following/unfollowing users works'''
+    def setUp(self):
+        super().setUp()
+
+        f1 = Follows(user_being_followed_id=self.u1_id,
+                    user_following_id=self.u2_id)
+
+        db.session.add(f1)
+        db.session.commit()
+
+    def test_show_following(self):
+        '''Test to show that correct users are shown in following page'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u2_id
+
+            resp = c.get(f'/users/{self.u2_id}/following')
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u1", html)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_show_followers(self):
+        '''Test to show that correct users are shown in followers page'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u2_id
+
+            resp = c.get(f'/users/{self.u1_id}/followers')
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u2", html)
+            self.assertEqual(resp.status_code, 200)
+
+
+    def test_follow_button(self):
+        '''Test to show follow button works'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.post(f'/users/follow/{self.u2_id}', follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u2", html)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_unfollow_button(self):
+        '''Test to show that unfollow button works'''
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u2_id
+
+            resp = c.post(f'/users/follow/{self.u1_id}', follow_redirects=True)
+
+            html = resp.get_data(as_text=True)
+
+            self.assertIn("@u2", html)
+            self.assertEqual(resp.status_code, 200)
